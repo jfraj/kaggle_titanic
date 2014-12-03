b4a90fd859d2cb9a23e6b4851738b6a45ef27cc9
jfraj <rajotte.jeanfrancois@gmail.com>
2014-12-03 17:38:45 -0500

    modifying clean_data again s.t. it can clean other things than just self.df_train

--- rforest.py
+++ rforest.py
@@ -19,14 +19,15 @@ class RandomForestModel(object):
         # Turn data in pandas dataframe
         self.df_train = pd.read_csv(train_data_fname)
 
-    def clean_data(self):
+    def clean_data(self, df):
         """
         Cleans self.df_train
         """
         ## Sex
         ## female = 0, male = 1
         gender_dict = {'female': 0, 'male': 1}
-        self.df_train.loc[:,'Gender'] = self.df_train.loc[:,'Sex'].map(gender_dict).astype(int)
+        #self.df_train.loc[:,'Gender'] = self.df_train.loc[:,'Sex'].map(gender_dict).astype(int)
+        df.loc[:,'Gender'] = df.loc[:,'Sex'].map(gender_dict).astype(int)
 
         ## Port
         # Embarked (at port) from 'C', 'Q', 'S'
@@ -34,9 +35,9 @@ class RandomForestModel(object):
         # Replace NA with most frequent value
         # DataFrame.mode() returns the most frequent object in a set
         # here Embarked.mode.values is a numpy.ndarray type (what pandas use to store strings) 
-        if len(self.df_train.Embarked[self.df_train.Embarked.isnull() ]) > 0:
-            most_common_value = self.df_train.Embarked.dropna().mode().values[0]
-            self.df_train.loc[self.df_train.Embarked.isnull(),'Embarked'] = most_common_value 
+        if len(df.Embarked[df.Embarked.isnull() ]) > 0:
+            most_common_value = df.Embarked.dropna().mode().values[0]
+            df.loc[df.Embarked.isnull(),'Embarked'] = most_common_value 
 
         # The following lines produce [(0, 'C'), (1, 'Q'), (2, 'S')]
         #Ports = list(enumerate(np.unique(df['Embarked'])))
@@ -44,7 +45,7 @@ class RandomForestModel(object):
         #Ports_dict = { name : i for i, name in Ports }
         Ports_dict = {'Q': 1, 'C': 0, 'S': 2}
         # Converting port string as port int
-        self.df_train.Embarked = self.df_train.Embarked.map( lambda x: Ports_dict[x]).astype(int)
+        df.Embarked = df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
 
         ## Age
         ##Need to be updated by the class/gender medians
@@ -55,36 +56,36 @@ class RandomForestModel(object):
         ## Get the median ages
         for i in range(0,2):
             for j in range(0, 3):
-                median_ages[i, j] = self.df_train[(self.df_train['Gender']==i) & (self.df_train['Pclass']==j+1)]['Age'].dropna().median()
+                median_ages[i, j] = df[(df['Gender']==i) & (df['Pclass']==j+1)]['Age'].dropna().median()
         ## Fill the median age to na
         for i in range(0,2):
             for j in range(0,3):
-                self.df_train.loc[ (self.df_train.Age.isnull()) & (self.df_train.Gender == i) & (self.df_train.Pclass == j+1),'Age'] = median_ages[i, j]
+                df.loc[ (df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j+1),'Age'] = median_ages[i, j]
 
         ## Fare
         # All the missing Fares -> assume median of their respective class
-        if len(self.df_train.Fare[ self.df_train.Fare.isnull() ]) > 0:
+        if len(df.Fare[ df.Fare.isnull() ]) > 0:
             median_fare = np.zeros(3)
             for f in range(0,3):
-                median_fare[f] = self.df_train[ self.df_train.Pclass == f+1 ]['Fare'].dropna().median()
+                median_fare[f] = df[ df.Pclass == f+1 ]['Fare'].dropna().median()
             for f in range(0,3):
-                self.df_train.loc[ (self.df_train.Fare.isnull()) & (self.df_train.Pclass == f+1 ), 'Fare'] = median_fare[f]
+                df.loc[ (df.Fare.isnull()) & (df.Pclass == f+1 ), 'Fare'] = median_fare[f]
 
         ## Ticket
         ## Tickets are like A/5 21171, let's use the last part as a number
         ticket_number_list=[]
-        for iticket in self.df_train.Ticket:
+        for iticket in df.Ticket:
             if iticket.split()[-1].isdigit():
                 ticket_number_list.append(int( iticket.split()[-1]))
             else:
                 ticket_number_list.append(-100000)#Maybe NA would be a better default
         #df['Ticket_number'] = ticket_number_list
-        self.df_train.loc[:,'Ticket_number'] = ticket_number_list
+        df.loc[:,'Ticket_number'] = ticket_number_list
 
     def trainNselfCheck(self, train_fraction = 1):
         """
         Train the model on a fraction of the data and check on another fraction
-        Warning: this cleans the data frame self.df_train
+        Warning: this will affect self.df_train because it calls self.clean_data(self.df_train)
         """
 
         # Separate the training sample into two samples
@@ -97,8 +98,11 @@ class RandomForestModel(object):
 
         ## Training set
         ## Data clean up for training
-        self.clean_data()
-        self.df_train = self.df_train.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1)
+        self.clean_data(self.df_train)
+        #self.df_train = self.df_train.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1)
+        #self.df_train = self.df_train.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId', 'Embarked'], axis=1)
+        self.df_train = self.df_train.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId', 'Parch', 'SibSp', 'Embarked'], axis=1)
+        #self.df_train = self.df_train.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId', 'Age', 'Fare', 'Ticket_number', 'Gender'], axis=1)
         ## Convert to numpy array
         train_data = self.df_train.values
         train_data = train_data[:ntrain,:]
@@ -110,15 +114,18 @@ class RandomForestModel(object):
 
 
         ## Determine the score based of multiple cross validation
-        scores = cross_validation.cross_val_score(forest, train_data[0:,1:], train_data[0:,0], cv=5)
+        scores = cross_validation.cross_val_score(forest, train_data[0:,1:], train_data[0:,0], cv=25)
 
         #predictions = forest.predict(validation_data[0::,1::]).astype(int)
         print '\nScores'
         print scores
         print "\n\nCross_validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
+
+        print '\nFeatures importances'
+        for iimpotance, ifeature in zip(forest.feature_importances_, list(self.df_train.columns.values)[1:]):
+            print '{0} \t: {1} '.format(ifeature, round(iimpotance, 2))
         #raw_input('ok...')
         return {'scores': scores}
-
 
 
 if __name__=='__main__':
